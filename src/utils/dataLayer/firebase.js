5b9d84ad22fdafc965e311889e79a90dbeec681f
Phan Thi To Quyen <87388349+ToQuyenPhan@users.noreply.github.com>
2023-02-19 21:28:47 +0700

    Update firebase.js

--- src/utils/dataLayer/firebase.js
+++ src/utils/dataLayer/firebase.js
@@ -5,13 +5,12 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDVmzFWGgFf5Mh9uwpyV1MHsKWT40hO_PQ',
-  authDomain: 'unics-e46a4.firebaseapp.com',
-  projectId: 'unics-e46a4',
-  storageBucket: 'unics-e46a4.appspot.com',
-  messagingSenderId: '1000685570084',
-  appId: '1:1000685570084:web:ca6f7a4b3cad26de19b0b1',
-  measurementId: 'G-Z73T8JVKMR',
+  apiKey: 'AIzaSyDtu4VForkQtHpNrxOx6N3PM_TwsiEB2rs',
+  authDomain: 'uniclubproject.firebaseapp.com',
+  projectId: 'uniclubproject',
+  storageBucket: 'uniclubproject.appspot.com',
+  messagingSenderId: '522814460221',
+  appId: '1:522814460221:web:d679d7d9c0da7c31e67554',
 };
 
 // Initialize Firebase
